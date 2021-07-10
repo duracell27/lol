493285980340b8cf8f41f669f4a6e93c46a051ed
duracell27 <volodymyrshmidt@gmail.com>
2021-07-11 00:08:22 +0300

    add timers add  manuring prices and reduce time

--- src/componets/Field.js
+++ src/componets/Field.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { FieldContext } from "../context/field/fieldContext";
 import { GlobalContext } from "../context/global/globalContext";
@@ -17,6 +17,22 @@ export const Field = ({ fieldInfo }) => {
     2: fieldInfo.manuring ? `Удобрити ${fieldInfo.manuring.name}` : `Удобрити`,
     3: "Зібрати",
     4: "Вскопати",
+  };
+
+  const [disable, setDisable] = useState(false);
+  const [disableTime, setDisableTime] = useState(null);
+  const [timePlant, settimePlant] = useState(null);
+  const timer = (plusTime) => {
+    setDisable(true);
+    setDisableTime(plusTime);
+    const newTime = Math.round(new Date() / 1000) + plusTime;
+    const inter = setInterval(() => {
+      setDisableTime(newTime - Math.round(new Date() / 1000));
+      if (newTime === Math.round(new Date() / 1000)) {
+        clearInterval(inter);
+        setDisable(false);
+      }
+    }, 1000);
   };
 
   return (
@@ -44,16 +60,48 @@ export const Field = ({ fieldInfo }) => {
             : `${fieldInfo.plant.name}`}
         </h2>
 
-        <button className={cls.button}
+        <button
+          disabled={disable}
+          className={cls.button}
           onClick={() => {
-            if (actionNames[fieldInfo.actionId] === "Посадити") {
+            if (
+              actionNames[fieldInfo.actionId] === "Посадити" ||
+              actionNames[fieldInfo.actionId] === "Удобрити"
+            ) {
               history.push("/chosePlant");
             } else {
               if (fieldInfo.actionId === 0) {
                 removeGold(fieldInfo.plant.buyPrice);
+                settimePlant(Math.round(new Date() / 1000));
+                const chekForWaterButton =
+                  timePlant +
+                  fieldInfo.plant.timeWater -
+                  Math.round(new Date() / 1000);
+                if (chekForWaterButton > 0) {
+                  timer(chekForWaterButton);
+                }
+                timer(fieldInfo.plant.timeWater);
+              }
+              if (fieldInfo.actionId === 1) {
+                const chekForManuringButton =
+                  timePlant +
+                  fieldInfo.plant.timeManuring -
+                  Math.round(new Date() / 1000);
+                if (chekForManuringButton > 0) {
+                  timer(chekForManuringButton);
+                }
+              }
+              if (fieldInfo.actionId === 2) {
+                removeGold(fieldInfo.manuring.buyPrice);
+                const chekForHarvestButton =
+                  timePlant +
+                  fieldInfo.plant.timeHarvest -
+                  Math.round(new Date() / 1000 + fieldInfo.manuring.timeReduce);
+                if (chekForHarvestButton > 0) {
+                  timer(chekForHarvestButton);
+                }
               }
               if (fieldInfo.actionId === 3) {
-                console.log("зібрано", fieldInfo.plant.name);
                 addToWerehouse(fieldInfo.plant);
               }
               addExp(defPlant.exp[fieldInfo.actionId]);
@@ -61,7 +109,8 @@ export const Field = ({ fieldInfo }) => {
             }
           }}
         >
-          {actionNames[fieldInfo.actionId]}
+          {actionNames[fieldInfo.actionId]}{" "}
+          {disableTime ? `${disableTime} сек.` : null}
         </button>
       </div>
     </div>
