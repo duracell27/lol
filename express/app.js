9f11c0502c3d7415cc7c2caeb2b5f2a045567305
duracell27 <volodymyrshmidt@gmail.com>
2021-07-15 16:01:28 +0300

    express add, reg login ready

---
+++ express/app.js
@@ -0,0 +1,27 @@
+process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = config.get("port") || 5000;
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log("Server error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ express/routes/auth.routes.js
@@ -0,0 +1,81 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("./../models/User");
+const { check, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken')
+const config = require("config");
+process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
+const router = Router();
+
+router.post(
+  "/register",
+  [
+    check("email", "Не правильний email").isEmail(),
+    check("password", "Мінімальна довжина паролю 6 символів").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const erorrs = validationResult(req);
+      if (!erorrs.isEmpty()) {
+        return res
+          .status(400)
+          .json({ erorrs: erorrs.array(), message: "Не коректні дані при геєстрації" });
+      }
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email: email });
+
+      if (candidate) {
+        return res.status(400).json({ message: "Такий фермер вже існує" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const user = new User({ email: email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: "Вітаю! Ви тепер фермер" });
+    } catch (e) {
+      res.status(500).json({ message: "Щось пішло не так, попробуйте знову" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Введіть коректний email").normalizeEmail().isEmail(),
+    check("password", "Введіть пароль").exists()
+  ],
+  async (req, res) => {
+    try {
+      const erorrs = validationResult(req);
+      if (!erorrs.isEmpty()) {
+        return res
+          .status(400)
+          .json({ erorrs: erorrs.array(), message: "Не коректні дані при вході" });
+      }
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json({ message: "Такий фермер не знайдений" });
+      }
+    
+      const isMatch = await bcrypt.compare(password, user.password)
+      if(!isMatch){
+        res.status(400).json({ message: "Не правильний пароль" });
+      }
+
+      const token = jwt.sign({userId: user.id}, config.get('jwtSecret'),{expiresIn: '1h'})
+      res.json({token: token, userId: user.id})
+    } catch (e) {
+      res.status(500).json({ message: "Щось пішло не так, попробуйте знову" });
+    }
+  }
+);
+
+module.exports = router;
